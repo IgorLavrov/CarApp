e30b3d6be6dff1496524426e8be3882a2e731994
DESKTOP-NEQI063\Laptop <igor.lavrov90@hotmail.co.uk>
2023-12-28 22:12:58 +0200

    views created

--- CarApp/Models/Car/CarDetailsViewModel.cs
+++ CarApp/Models/Car/CarDetailsViewModel.cs
@@ -2,5 +2,18 @@
 {
     public class CarDetailsViewModel
     {
+        public Guid? Id { get; set; }
+        public string Owner { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string VinNumber { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string TypeOfFuel { get; set; }
+        public int EngineCapacity { get; set; }
+        public int NumberOfCarDoors { get; set; }
+        public int CarWeight { get; set; }
+        public DateTime BuiltAt { get; set; }
+        public DateTime RegistratedAt { get; set; }
     }
 }
