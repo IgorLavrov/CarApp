c19060e819f8927f8539672eed081e10d983f18f
DESKTOP-NEQI063\Laptop <igor.lavrov90@hotmail.co.uk>
2023-12-28 22:36:03 +0200

    Car.Core Completed

---
+++ CarApp.Core/Dto/CarAppDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp.Core.Dto
+{
+    public class CarAppDto
+    {
+        public Guid? Id { get; set; }
+        public string Owner { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string VinNumber { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string TypeOfFuel { get; set; }
+        public int EngineCapacity { get; set; }
+        public int NumberOfCarDoors { get; set; }
+        public int CarWeight { get; set; }
+        public DateTime BuiltAt { get; set; }
+        public DateTime RegistratedAt { get; set; }
+
+
+    }
+}
---
+++ CarApp.Core/ServiceInterface/ICarAppServices.cs
@@ -0,0 +1,21 @@
+﻿using CarApp.Core.Dto;
+using CarApp.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp.Core.ServiceInterface
+{
+    public interface ICarAppServices
+    {
+        Task<CarsApp> GetAsync(Guid id);
+        Task<CarsApp> Create(CarAppDto dto);
+        Task<CarsApp> Delete(Guid id);
+        Task<CarsApp> Update(CarAppDto dto);
+    }
+
+
+}
+
